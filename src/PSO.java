e5225b64ea13d335f76026be7c79896fe6a0059d
MichalBogusz <mbogusz998@gmail.com>
2020-06-05 22:16:14 +0200

    added execution time projection

--- src/PSO.java
+++ src/PSO.java
@@ -1,13 +1,21 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class PSO {
     public static void main(String[] argv) {
         final double minRange = -5.12;
         final double maxRange = 5.12;
-        final int numOfParticles = 1000;
+        final int numOfParticles = 1000000;
         final int numOfEpochs = 20;
         final double inertia = 0.9;
         final double localBestAim = 2.5;
         final double globalBestAim = 2.5;
         Swarm PSO = new Swarm(minRange, maxRange, numOfParticles, numOfEpochs, inertia, localBestAim, globalBestAim);
+        
+        Instant start = Instant.now();
         PSO.startAlgorithm();
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.printf("Time of execution  %dms\n", timeElapsed);
     }
 }
